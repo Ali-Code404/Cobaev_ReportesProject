b7c5b42397703fd3cb6e465cb6a57c03cd4975b4
Omar Alí <alicode404@gmail.com>
2023-10-19 19:45:11 -0600

    First commit
    Commit about all files

---
+++ dashboard/index.php
@@ -0,0 +1,21 @@
+<?php require_once "vistas/parte_superior.php"?>
+
+<!--INICIO del cont principal-->
+<div class="container-fluid">
+    
+    <div class="jumbotron">
+  <h1>Manual del usuario</h1>
+  <p>Dentro del sistema de reportes y citatorios se encuentran 5 apartados.</p>
+    <ul>
+     <li>Inicio: En este apartado encontrarás la guia de usuario, cómo crear un justificante, añadir alumnos, dar de baja alumnos y generar reportes</li><br>
+     <li>Lista de alumnos: El apartado de lista de alumnos permite crear/registrar alumnos dentro del sistema de reportes y citatorios, buscar alumnos mediante nombre, semestre, grupo, entre otros datos, ordenar la tabla de menor a mayor, mostrar registros de 10, 25, 50 y 100 datos en la tabla y editar datos del alumno.</li><br>
+     <li>Generar justificantes: El apartado de generar justificantes permite realizar operaciones tales como buscar registros mediante cualquier dato dentro de la tabla, observar registros de 10, 25, 50 y 100 datos, generar justificantes ya que se te abrirá una ventana nueva en dónde tienes que llenar los campos para poder generar un justificante y así imprimir el formato PDF.</li><br>
+     <li>Generar reportes: El apartado de generar reportes permite generar reportes a los alumnos que se seleccionen mediante su ID, al presionar el botón de generar reporte se abrirá una ventana nueva para poder llenar la información requerida y así generar un reporte, tambien permite buscar alumnos mediante sus datos dentro de la tabla, ordenar registros de menor a mayor.</li><br>
+        <li>Lista de reportes: La lista de reportes nos muestra la tabla de reportes generados, buscar reportes mediante cualquier campo dentro de la tabla, ordenar registros y mostrar cierto numero de registros.</li>
+    </ul>
+</div>
+    
+</div>
+<!--FIN del cont principal-->
+
+<?php require_once "vistas/parte_inferior.php"?>
